ea08b8e4cd2a558d8ef47cce2d4b45e42b0300a3
SorokinN <nikolay@sorokin.dev>
2020-11-10 01:53:15 +0300

    Added new viewcontroler

---
+++ Game_NBA_Players/PlayerDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PlayerDetailsViewController.swift
+//  Game_NBA_Players
+//
+//  Created by Nikolay Sorokin on 10.11.20.
+//
+
+import UIKit
+
+class PlayerDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
