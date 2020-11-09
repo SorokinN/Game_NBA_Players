e31c3b5b3bf28992da38d6ac733c7750410c3bd0
SorokinN <nikolay@sorokin.dev>
2020-11-10 00:07:38 +0300

    Added Custom ViewControler

---
+++ Game_NBA_Players/PlayersViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlayersViewController.swift
+//  Game_NBA_Players
+//
+//  Created by Nikolay Sorokin on 9.11.20.
+//
+
+import UIKit
+
+class PlayersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Game_NBA_Players/ViewController.swift
+++ Game_NBA_Players/ViewController.swift
@@ -1,19 +1,1 @@
-//
-//  ViewController.swift
-//  Game_NBA_Players
-//
-//  Created by Nikolay Sorokin on 9.11.20.
-//
-
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
 
